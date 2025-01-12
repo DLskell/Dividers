4135c27feff7400fab422966ce1537948277569c
Louka Duflos <156593512+DLskell@users.noreply.github.com>
2025-01-12 19:54:51 +0100

    Update Dividers.py
    Just an easier and smaller way to do it

--- Dividers.py
+++ Dividers.py
@@ -1,9 +1,5 @@
 #enter a number and it will return all its positive divisors
 a=int(input("Value : "))
-b=0
-c=1
-while b!=a:
-    b=b+1
-    c=a%b
-    if c==0:
-        print(b)
+for b in range(2, a):
+    c = a % b
+    if c == 0: print(b)
